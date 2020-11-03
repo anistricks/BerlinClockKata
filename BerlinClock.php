4d5c027bd682a2465f0b99ae1a4db9e08c557b32
anis.assaidi@student.vinci.be <you@example.com>
2020-11-03 15:34:56 +0100

    add - test_minutes_given00_shouldReturnOOOO

--- BerlinClock.php
+++ BerlinClock.php
@@ -4,4 +4,8 @@
 class BerlinClock
 {
 
+    public function minutes(string $minutes) : string
+    {
+        if($minutes == 0) return "OOOO";
+    }
 }
--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -1,9 +1,17 @@
 <?php
-
+require "vendor/autoload.php";
+require "BerlinClock.php";
 
 use PHPUnit\Framework\TestCase;
 
 class BerlinClockTest extends TestCase
 {
+    public function test_minutes_given00_shouldReturnOOOO() {
+        $BerlinClock = new BerlinClock();
+
+        $actual = $BerlinClock->minutes("00");
+
+        self::assertEquals("OOOO", $actual);
+    }
 
 }
